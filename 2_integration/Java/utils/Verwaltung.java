2813d88c1e2ee8cf37d917e014d64d583584c9ce
zaza <eldakar@students.uni-marburg.de>
2023-06-04 15:14:24 +0200

    .

--- 2_integration/Java/utils/Verwaltung.java
+++ 2_integration/Java/utils/Verwaltung.java
@@ -18,7 +18,7 @@ public class Verwaltung {
     }
     private void parselaenderCsvFile() {
         String current = "";
-        String fileName="/home/zaza/Desktop/Work/Data Integration/umr-data-integration-project/0_datasets/durchschnitsgewicht_Lander.csv";
+        String fileName="/home/zaza/Desktop/Work/umr-data-integration-project/0_datasets/durchschnitsgewicht_Lander.csv";
         Dataholder dataholder= Dataholder.getInstance();
         try {
 
@@ -45,7 +45,7 @@ public class Verwaltung {
     }
     private void parseExerciseCsvFile() {
         String current = "";
-        String fileName="/home/zaza/Desktop/Work/Data Integration/umr-data-integration-project/0_datasets/exercise_dataset1.csv";
+        String fileName="/home/zaza/Desktop/Work/umr-data-integration-project/0_datasets/exercise_dataset1.csv";
         Dataholder dataholder= Dataholder.getInstance();
         try {
 
@@ -72,7 +72,7 @@ public class Verwaltung {
     }
     private void parseMcdonaldsCsvFile() {
         String current = "";
-        String fileName="/home/zaza/Desktop/Work/Data Integration/umr-data-integration-project/0_datasets/McDonalds_Nahrwertetabelle_fur_alle_Gerichte.csv";
+        String fileName="/home/zaza/Desktop/Work/umr-data-integration-project/0_datasets/McDonalds_Nahrwertetabelle_fur_alle_Gerichte.csv";
         Dataholder dataholder= Dataholder.getInstance();
         try {
 
